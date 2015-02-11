b0b714baf649e8a7c52a71c8ce9d5c0c97a93c22
Wakaba <wakaba@suikawiki.org>
2015-02-11 21:41:25 +0900

    canonical data format

---
+++ lib/TR/TextEntry.pm
@@ -0,0 +1,48 @@
+package TR::TextEntry;
+use strict;
+use warnings;
+
+sub new_from_text_id_and_source_text ($$$) {
+  my ($class, $id, $text) = @_;
+  my $self = bless {text_id => $id}, $class;
+  my $props = $self->{props} = {};
+  $text =~ s/\x0D\x0A/\x0A/g;
+  for (split /\x0A/, $text) {
+    if (/\A([0-9A-Za-z_]+):(.*)\z/) {
+      my ($n, $v) = ($1, $2);
+      $n =~ s/\\([nr\\])/{n => "\x0A", r => "\x0D", "\\" => "\\"}->{$1}/ge;
+      $props->{$n} = $v;
+    }
+  }
+  return $self;
+} # new_from_text_id_and_source_text
+
+sub text_id ($) {
+  return $_[0]->{text_id};
+} # text_id
+
+sub get ($$) {
+  return $_[0]->{props}->{$_[1]}; # or undef
+} # get
+
+sub set ($$$) {
+  if (defined $_[2]) {
+    $_[0]->{props}->{$_[1]} = $_[2];
+  } else {
+    delete $_[0]->{props}->{$_[1]};
+  }
+} # set
+
+sub as_source_text ($) {
+  my $self = $_[0];
+  my $props = $self->{props};
+  my @s;
+  for (sort { $a cmp $b } keys %$props) {
+    my $s = $_ . ':' . $props->{$_};
+    $s =~ s/([\x0D\x0A\\])/{"\x0D" => "\\r", "\x0A" => "\\n", "\\" => "\\\\"}->{$1}/ge;
+    push @s, $s;
+  }
+  return join "\x0A", @s;
+} # as_source_text
+
+1;
--- sketch/git.pl
+++ sketch/git.pl
@@ -3,6 +3,7 @@ use warnings;
 use Path::Tiny;
 use AnyEvent;
 use TR::TextRepo;
+use TR::TextEntry;
 
 my $global = AE::cv;
 
@@ -13,7 +14,10 @@ my $tr = TR::TextRepo->new_from_temp_path ($temp_path);
 $tr->texts_dir ('hoge/fuga');
 $tr->clone_by_url ($url)->then (sub {
   my $id = $tr->generate_text_id;
-  $tr->write_file_by_text_id_and_suffix ($id, 'ja.txt' => qq<\x{6000}abc>);
+  my $te = TR::TextEntry->new_from_text_id_and_source_text ($id, '');
+  $te->set (body_o => "\x{5000}\x0Dabc\x0Axy\x0A\x0A");
+  $te->set (Hoge => "aba de\x09\\");
+  $tr->write_file_by_text_id_and_suffix ($id, 'ja.txt' => $te->as_source_text);
 })->then (sub {
   $tr->text_ids->then (sub {
     warn join "\n", keys %{$_[0]};
