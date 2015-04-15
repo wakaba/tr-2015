082ba036c425aa33680d08359c23e78d27cb5044
Wakaba <wakaba@suikawiki.org>
2015-04-15 12:49:52 +0900

    TEON

--- lib/TR/TextEntry.pm
+++ lib/TR/TextEntry.pm
@@ -1,114 +1,56 @@
 package TR::TextEntry;
 use strict;
 use warnings;
-use Unicode::UTF8 qw(decode_utf8);
-
-sub _e ($) {
-  my $s = $_[0];
-  $s =~ s/([\x0D\x0A\\])/{"\x0D" => "\\r", "\x0A" => "\\n", "\\" => "\\\\"}->{$1}/ge;
-  return $s;
-} # _e
-
-sub _ec ($) {
-  my $s = $_[0];
-  $s =~ s/([\x0D\x0A:\\])/{"\x0D" => "\\r", "\x0A" => "\\n", ":" => "\\C", "\\" => "\\\\"}->{$1}/ge;
-  return $s;
-} # _ec
-
-sub _ue ($) {
-  return $_[0] unless index ($_[0], '\\') > -1;
-  my $v = $_[0];
-  $v =~ s/\\([nrC\\])/{n => "\x0A", r => "\x0D", "C" => ":", "\\" => "\\"}->{$1}/ge;
-  return $v;
-} # _ue
+use TEON;
 
 sub new_from_source_bytes ($$) {
-  return shift->new_from_source_text (decode_utf8 $_[0]);
+  return bless TEON->parse_bytes ($_[1]), $_[0];
 } # new_from_source_bytes
 
 sub new_from_source_text ($$) {
-  my ($class, $text) = @_;
-  my $self = bless {}, $class;
-  my $props = $self->{props} = {};
-  my $enum_props = $self->{enum_props} = {};
-  my $list_props = $self->{list_props} = {};
-  $text =~ s/\x0D\x0A/\x0A/g;
-  for (split /\x0A/, $text) {
-    if (/\A\$([^:]+):(.*)\z/) {
-      my ($n, $v) = ($1, $2);
-      $props->{_ue $n} = _ue $v;
-    } elsif (/\A&([^:]+):(.*)\z/) {
-      my ($n, $v) = (_ue $1, _ue $2);
-      $enum_props->{$n}->{$v} = 1;
-    } elsif (/\A\@([^:]+):(.*)\z/) {
-      my ($n, $v) = (_ue $1, _ue $2);
-      push @{$list_props->{$n} ||= []}, $v;
-    }
-  }
-  return $self;
+  return bless TEON->parse_text ($_[1]), $_[0];
 } # new_from_source_text
 
 sub get ($$) {
-  return $_[0]->{props}->{$_[1]}; # or undef
+  return $_[0]->{scalars}->{$_[1]}; # or undef
 } # get
 
 sub set ($$$) {
   if (defined $_[2]) {
-    $_[0]->{props}->{$_[1]} = $_[2];
+    $_[0]->{scalars}->{$_[1]} = $_[2];
   } else {
-    delete $_[0]->{props}->{$_[1]};
+    delete $_[0]->{scalars}->{$_[1]};
   }
 } # set
 
 sub set_or_delete ($$$) {
   if (defined $_[2] and length $_[2]) {
-    $_[0]->{props}->{$_[1]} = $_[2];
+    $_[0]->{scalars}->{$_[1]} = $_[2];
   } else {
-    delete $_[0]->{props}->{$_[1]};
+    delete $_[0]->{scalars}->{$_[1]};
   }
 } # set_or_delete
 
 sub enum ($$) {
-  return $_[0]->{enum_props}->{$_[1]} ||= {};
+  return $_[0]->{enums}->{$_[1]} ||= {};
 } # enum
 
 sub list ($$) {
-  return $_[0]->{list_props}->{$_[1]} ||= [];
+  return $_[0]->{lists}->{$_[1]} ||= [];
 } # list
 
 sub as_source_text ($) {
-  my $self = $_[0];
-  my $props = $self->{props};
-  my @s;
-  for (sort { $a cmp $b } keys %$props) {
-    my $s = '$' . (_ec $_) . ':' . _e $props->{$_};
-    push @s, $s;
-  }
-  my $enum_props = $self->{enum_props};
-  for my $key (sort { $a cmp $b } keys %$enum_props) {
-    for (sort { $a cmp $b } grep { $enum_props->{$key}->{$_} } keys %{$enum_props->{$key}}) {
-      my $s = '&' . (_ec $key) . ':' . _e $_;
-      push @s, $s;
-    }
-  }
-  my $list_props = $self->{list_props};
-  for my $key (sort { $a cmp $b } keys %$list_props) {
-    for my $item (@{$list_props->{$key}}) {
-      my $s = '@' . (_ec $key) . ':' . _e $item;
-      push @s, $s;
-    }
-  }
-  return join "\x0A", @s;
+  return TEON->to_text ($_[0]);
 } # as_source_text
 
 sub as_jsonalizable ($) {
   my $self = $_[0];
-  my $json = {%{$self->{props}}};
-  for my $key (keys %{$self->{enum_props}}) {
-    $json->{$key} = [sort { $a cmp $b } grep { $self->{enum_props}->{$key}->{$_} } keys %{$self->{enum_props}->{$key}}];
+  my $json = {%{$self->{scalars}}};
+  for my $key (keys %{$self->{enums}}) {
+    $json->{$key} = [sort { $a cmp $b } grep { $self->{enums}->{$key}->{$_} } keys %{$self->{enums}->{$key}}];
   }
-  for my $key (keys %{$self->{list_props}}) {
-    $json->{$key} = $self->{list_props}->{$key};
+  for my $key (keys %{$self->{lists}}) {
+    $json->{$key} = $self->{lists}->{$key};
   }
   delete $json->{tags} if defined $json->{tags} and not 'ARRAY' eq ref $json->{tags};
   delete $json->{args} if defined $json->{args} and not 'ARRAY' eq ref $json->{args};
@@ -129,7 +71,7 @@ It could be difficult to manually merge conflicting branches.
 
 =head1 LICENSE
 
-Copyright 2014 Wakaba <wakaba@suikawiki.org>.
+Copyright 2015 Wakaba <wakaba@suikawiki.org>.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
